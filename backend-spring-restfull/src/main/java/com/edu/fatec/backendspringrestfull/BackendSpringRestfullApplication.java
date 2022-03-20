e2726fa2f058e4b5009ed81d01e961e9475c1d3a
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2022-03-20 07:37:18 -0300

    Inclusão do projeto de integração entre frontend react e backend spring

---
+++ backend-spring-restfull/src/main/java/com/edu/fatec/backendspringrestfull/BackendSpringRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.edu.fatec.backendspringrestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringRestfullApplication.class, args);
+	}
+
+}
---
+++ backend-spring-restfull/src/test/java/com/edu/fatec/backendspringrestfull/BackendSpringRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.fatec.backendspringrestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
